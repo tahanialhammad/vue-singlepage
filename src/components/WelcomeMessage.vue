20a02f694c958bdd3ec1c9bdf1d0c9e5565dc90c
tahanialhammad <tahani_alhammad@yahoo.com>
2021-03-08 23:17:10 +0100

    install vue cli

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="welcome-message">
+        <h1> welcome to vue js </h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia necessitatibus ipsum ab alias voluptatibus beatae in consectetur quis culpa suscipit dicta accusamus magnam dolorum, sapiente ex eum cum at fugit.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeMessage"
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>from welcomemessage component</h1>
+    <WelcomeMessage/>
+  </div>
+</template>
+
+<script>
+import WelcomeMessage from "@/components/WelcomeMessage.vue";
+
+export default {
+  name: "about",
+  components: {
+    WelcomeMessage
+  }
+};
+</script>
